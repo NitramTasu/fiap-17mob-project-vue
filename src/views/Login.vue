16195d9da7345fb8338492addff1ed84db8426c8
Martin Schwans <amrastasu@gmail.com>
2019-05-21 18:32:08 -0300

    Criada a tela de login (apenas front).

---
+++ src/views/Login.vue
@@ -0,0 +1,129 @@
+<template>
+  <div>
+    <form novalidate class="md-layout" @submit.prevent="validateUser">
+      <md-card class="md-layout-item md-size-50 md-small-size-100">
+        <md-card-header>
+          <div class="md-title">Users</div>
+        </md-card-header>
+
+        <md-card-content>
+          <md-field :class="getValidationClass('email')">
+            <label for="email">Email</label>
+            <md-input
+              type="email"
+              name="email"
+              id="email"
+              autocomplete="email"
+              v-model="form.email"
+              :disabled="sending"
+            />
+            <span class="md-error" v-if="!$v.form.email.required">Campo E-mail obrigatório</span>
+            <span class="md-error" v-else-if="!$v.form.email.email">E-mail inválido</span>
+          </md-field>
+
+          <md-field :class="getValidationClass('password')">
+            <label for="password">Senha</label>
+            <md-input
+              type="password"
+              name="password"
+              id="password"
+              v-model="form.password"
+              :disabled="sending"
+            />
+            <span class="md-error" v-if="!$v.form.email.required">Campo senha obrigatório</span>
+            <span class="md-error" v-else-if="!$v.form.email.email">Senha inválida</span>
+          </md-field>
+        </md-card-content>
+
+        <md-progress-bar md-mode="indeterminate" v-if="sending"/>
+
+        <md-card-actions>
+          <md-button type="submit" class="md-primary" :disabled="sending">Logar</md-button>
+        </md-card-actions>
+      </md-card>
+
+      <md-snackbar
+        :md-active.sync="userSaved"
+      >Usuário {{ $v.form.email.email }} logado com sucesso!!</md-snackbar>
+    </form>
+  </div>
+</template>
+
+<script>
+import { validationMixin } from "vuelidate";
+import {
+  required,
+  email,
+  minLength,
+  maxLength
+} from "vuelidate/lib/validators";
+
+export default {
+  name: "FormValidation",
+  mixins: [validationMixin],
+  data: () => ({
+    form: {
+      password: null,
+      email: null
+    },
+    userSaved: false,
+    sending: false,
+    lastUser: null
+  }),
+  validations: {
+    form: {
+      password: {
+        required
+      },
+      email: {
+        required,
+        email
+      }
+    }
+  },
+  methods: {
+    getValidationClass(fieldName) {
+      const field = this.$v.form[fieldName];
+
+      if (field) {
+        return {
+          "md-invalid": field.$invalid && field.$dirty
+        };
+      }
+    },
+    clearForm() {
+      this.$v.$reset();
+
+      this.form.password = null;
+      this.form.email = null;
+    },
+    saveUser() {
+      this.sending = true;
+
+      // Instead of this timeout, here you can call your API
+      window.setTimeout(() => {
+        //this.lastUser = `${this.form.firstName} ${this.form.lastName}`;
+        this.userSaved = true;
+        this.sending = false;
+        this.clearForm();
+      }, 1500);
+    },
+    validateUser() {
+      this.$v.$touch();
+
+      if (!this.$v.$invalid) {
+        this.saveUser();
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.md-progress-bar {
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+}
+</style>
