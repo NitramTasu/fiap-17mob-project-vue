7456987f1f7a9847300c45c9cbc29045a17bb947
Martin Schwans <amrastasu@gmail.com>
2019-05-21 17:49:19 -0300

    Adicionado Vue Material

--- src/main.js
+++ src/main.js
@@ -1,15 +1,21 @@
-import '@babel/polyfill'
-import Vue from 'vue'
-import './plugins/bootstrap-vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
+import "@babel/polyfill";
+import Vue from "vue";
+import "./plugins/bootstrap-vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./registerServiceWorker";
 
-Vue.config.productionTip = false
+import VueMaterial from "vue-material";
+import "vue-material/dist/vue-material.min.css";
+import "vue-material/dist/theme/default.css";
+
+Vue.config.productionTip = false;
+
+Vue.use(VueMaterial);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
